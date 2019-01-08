86253c0682c56e408e7966f91e551fd9f3a30752
Pedro Henrique Alves de Azevedo <pedro-azevedo@outlook.com.br>
2019-01-08 09:17:24 -0200

    Adicionado relacionamento pedido produto ao ORM

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
     has_many :product_ingredients
     has_many :ingredients, :through => :product_ingredients
+
+    as_many :order_products
+    has_many :orders, :through => :order_products
 end
