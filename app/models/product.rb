909d06397d144d53386b6edc8109dee87c69df54
Pedro Henrique Alves de Azevedo <pedro-azevedo@outlook.com.br>
2019-01-08 10:10:58 -0200

    Correção relacionamento produto

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
     has_many :product_ingredients
     has_many :ingredients, :through => :product_ingredients
 
-    as_many :order_products
-    has_many :orders, :throgh => :order_products
+    has_many :order_products
+    has_many :orders, :through => :order_products
     has_one :type
 end
